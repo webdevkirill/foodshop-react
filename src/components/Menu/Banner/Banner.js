c6198346d6dbcabe5c0208eab66e7008f26d3750
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-10-21 23:12:03 +0300

    Добавил баннер для меню

---
+++ src/components/Menu/Banner/Banner.js
@@ -0,0 +1,9 @@
+import bannerImg from '../../../img/banner.png';
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+    width: 100%;
+    height: 10rem;
+    background: url(${bannerImg}) center no-repeat;
+    background-size: cover;
+`;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import dbMenu from '../../DBMenu';
+import { Banner } from './Banner/Banner';
 import { ListItem } from './ListItem/ListItem';
 import { MenuStyled, SectionMenu } from './MenuStyles';
 
 export const Menu = () => (
     <MenuStyled>
+        <Banner />
         <SectionMenu>
             <h2>Бургеры</h2>
             <ListItem itemList={dbMenu.burger} />
