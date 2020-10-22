ffa01dbc5643c289390822316956865fc3a2811f
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-10-22 22:26:32 +0300

    добавили вывод количества и правильной цены в списке заказов

--- src/components/Order/OrderListItem/OrderListItem.js
+++ src/components/Order/OrderListItem/OrderListItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ItemName, ItemPrice, OrderItemStyled, TrashButton } from './OrderListItemStyled';
-import { formatCurrency } from '../../../utils/utils';
+import { formatCurrency, totalPriceItems } from '../../../utils/utils';
 
 
 export const OrderListItem = ({order}) => (
     <OrderItemStyled>
         <ItemName>{order.name}</ItemName>
-        <span>2</span>
-        <ItemPrice>{formatCurrency(order.price)}</ItemPrice>
+        <span>{order.count}</span>
+        <ItemPrice>{formatCurrency(totalPriceItems(order))}</ItemPrice>
         <TrashButton />
     </OrderItemStyled>
 )
