34dc822952820dd910b6609e22d167ae0735d88e
webdevkirill <kirill-fjodorov000@yandex.ru>
2020-10-27 00:23:36 +0300

    Вынес базу данных в папку хостинга и написал хук для получения данных

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import dbMenu from '../../DBMenu';
 import { Banner } from './Banner/Banner';
 import { ListItem } from './ListItem/ListItem';
 import { MenuStyled, SectionMenu } from './MenuStyles';
+import { useFetch } from '../../hooks/useFetch';
 
-export const Menu = ({setOpenItem}) => (
-    <MenuStyled>
-        <Banner />
-        <SectionMenu>
-            <h2>Бургеры</h2>
-            <ListItem 
-            itemList={dbMenu.burger}
-            setOpenItem={setOpenItem}
-            />
-        </SectionMenu>
+export const Menu = ({setOpenItem}) => {
 
-        <SectionMenu>
-            <h2>Закуски / Напитки</h2>
-            <ListItem 
-            itemList={dbMenu.other}
-            setOpenItem={setOpenItem}
-            />
-        </SectionMenu>
-    </MenuStyled>
-)
+    const res = useFetch();
+    const dbMenu = res.response;
+    const error = res.error;
+    
+    return (
+        <MenuStyled>
+            <Banner />
+            {dbMenu ? 
+            <div>
+                <SectionMenu>
+                    <h2>Бургеры</h2>
+                    <ListItem 
+                    itemList={dbMenu.burger}
+                    setOpenItem={setOpenItem}
+                    />
+                </SectionMenu>
+
+                <SectionMenu>
+                    <h2>Закуски / Напитки</h2>
+                    <ListItem 
+                    itemList={dbMenu.other}
+                    setOpenItem={setOpenItem}
+                    />
+                </SectionMenu>
+            </div> : error ? 
+            <div>Что-то пошло не так :(</div> :
+            <div>Загрузка...</div>
+            }
+        </MenuStyled>
+    )
+}
