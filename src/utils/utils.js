05d4153f839dcdfc50f39f50f4a79e15c3d50156
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-10-26 22:24:22 +0300

    Инициализировал firebase и исправил ошибку при расчете цены с топпингами

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,7 +3,8 @@ export const formatCurrency = (price) => price.toLocaleString('ru-RU',
 )
 
 export const totalPriceItems = (order) => {
-    const countToppings = order.toppings ? order.toppings.filter(item => item.checked).length : 0;
+    console.log(order)
+    const countToppings = order.topping && order.topping.filter(item => item.checked).length;
     const priceToppings = order.price * 0.1 * countToppings;
 
     return (order.price + priceToppings) * order.count
