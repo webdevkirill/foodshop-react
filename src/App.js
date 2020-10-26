fc3a91a5f54d10fac92fc2834623e0310f4615c0
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-10-26 22:38:50 +0300

    Инициализировал firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ import { Order } from './components/Order/Order';
 import { GlobalStyle } from './GlobalStyle';
 import { useOpenItem } from './hooks/useOpenItem';
 import { useOrders } from './hooks/useOrders';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: "food-shop-react.firebaseapp.com",
+  databaseURL: "https://food-shop-react.firebaseio.com",
+  projectId: "food-shop-react",
+  storageBucket: "food-shop-react.appspot.com",
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
 
